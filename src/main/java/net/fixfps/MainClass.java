73d2db844379cafc42e7715bd726f16d8c67fff4
zDany01 <85403430+zDany01@users.noreply.github.com>
2021-07-14 01:50:10 +0200

    Added stickroll with temporaney texture

--- src/main/java/net/fixfps/MainClass.java
+++ src/main/java/net/fixfps/MainClass.java
@@ -1,13 +1,23 @@
 package net.fixfps;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class MainClass implements ModInitializer {
+	
+	public static final Item StickRoll = new Item(new Item.Settings().group(ItemGroup.MISC));
+	
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+
+		Registry.register(Registry.ITEM, new Identifier("fixfps","stickroll"), StickRoll);
+
 
 	}
 }
