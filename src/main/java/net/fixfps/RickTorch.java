60db7950c07c99ddef0e283d517aab272af84558
zDany01 <85403430+zDany01@users.noreply.github.com>
2021-07-16 00:44:27 +0200

    ended but torch is a block and not a torch :/

--- src/main/java/net/fixfps/RickTorch.java
+++ src/main/java/net/fixfps/RickTorch.java
@@ -10,17 +10,32 @@ import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 
 public class RickTorch extends Block {
 
-    public static int numrick = 0;
 
-    public RickTorch(Settings settings) {super(settings);}
+    protected RickTorch(Settings settings) {
+        super(settings);}
+
+
+    public static int numrick = 0;
 
     public void onPlaced(World world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack itemStack) {
         if (!world.isClient) {
-                world.playSound(null, pos, new SoundEvent(new Identifier("fixfps","rick" + (++numrick))), SoundCategory.BLOCKS, 1f, 1f);
+                world.playSound(null, pos, new SoundEvent(new Identifier("fixfps","rick" + (++numrick))), SoundCategory.PLAYERS, 0.1F, 1F);
+                if (numrick>=9){numrick = 0;}
         }
     }
+
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
+       return VoxelShapes.cuboid(0.40,0,0.40,0.60,0.65,0.60);
+    }
+    
     
 }
